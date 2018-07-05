dfd21397b68bc269447c5650f3c7e2cca3898893
Milu234 <milanhazra234.mh@gmail.com>
2018-07-05 12:36:41 +0530

    cart Completed

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Milan Store</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="js/jquery2.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class=" navbar-header">
+				<a href="#" class="navbar-brand">Milan</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-briefcase"></span>Product</a></li>
+				<li style="width: 300px;left: 10px;top: 10px;"><input type="text" class="form-control" id="search"></li>
+				<li style="top: 10px;left: 20px;"><input type="submit"  class="btn btn-primary" id="search_btn" ></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a>
+					<div class="dropdown-menu"></div>		
+
+
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>Login</a>
+					<ul class="dropdown-menu">
+						<div style="width: 300px;">
+							<div class="panel panel-primary">
+							<div class="panel-heading">Login</div>
+							<div class="panel-heading">
+								<label for="email">Email</label>
+								<input type="email" class="form-control" id="email" required/>
+								<label for="email">Password</label>
+								<input type="password" class="form-control" id="password" required/>
+								<p><br></p>
+								<a href="#" style="color: white; list-style: none;">Forgotten Password?..</a><input type="submit" class="btn btn-success" style="float: right;" id="Login" value="Login" >
+							</div>
+							<div class="panel-footer" id="e_msg"></div>
+						</div>
+					</div>
+					</ul>
+				</li>
+				<li><a href="#"><span class="glyphicon glyphicon-user"></span>Register</a></li>
+			</ul>
+		</div>
+		 
+	</div>
+	<p><br></p>
+	<p><br></p>
+	<p><br></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-2">
+				<div class="nav nav-pills nav-stacked" >
+					<li class="active"><a href="#"><h4>Categories</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div>
+
+				<div class="nav nav-pills nav-stacked" >
+					<li class="active"><a href="#"><h4>Brand</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div>
+			</div>
+				
+		<div class="col-md-8">
+			<div class="panel panel-info">
+				<div class="panel-heading">Products</div>
+				<div class="panel-body">
+					<div class="col-md-4">
+						<div class="panel panel-info">
+							<div class="panel-heading"><h4>One +6</h4></div>
+							<div class="panel-body">
+								<img  class="img-responsive" src="product_images/images.jpeg"/>
+							</div>
+							<div class="panel-heading">
+								Rs.12000
+								<button style="float: right;" class="btn btn-danger btn-xs">AddToCart</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">&copy; 2018</div>
+			</div>
+			
+		</div>
+		<div class="col-md-1">
+			
+		</div>	
+		</div>
+
+	</div>
+
+</body>
+</html>
